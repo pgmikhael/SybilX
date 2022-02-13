2663da0497ceb435d49f7763a863b74c83eef4f7
Peter Mikhael <pgmikhael@gmail.com>
2022-02-13 02:54:34 -0500

    make cohort2

--- sybilx/datasets/mgh.py
+++ sybilx/datasets/mgh.py
@@ -4,6 +4,7 @@ from ast import literal_eval
 from sybilx.datasets.nlst import NLST_Survival_Dataset
 from collections import Counter
 from sybilx.datasets.utils import fit_to_length, get_scaled_annotation_area
+from sybilx.utils.registry import register_object
 
 DEVICE_ID = {
     "GE MEDICAL SYSTEMS": 0,
@@ -17,9 +18,10 @@ DEVICE_ID = {
 }
 
 
+@register_object("mgh_cohort1", "dataset")
 class MGH_Dataset(NLST_Survival_Dataset):
     """
-    MGH Dataset
+    MGH Dataset Cohort 1
     """
 
     def create_dataset(self, split_group):
@@ -161,10 +163,6 @@ class MGH_Dataset(NLST_Survival_Dataset):
                         sample["has_annotation"] = (
                             np.sum(sample["volume_annotations"]) > 0
                         )
-                        sample["use_manual_annotation"] = (
-                            sample["has_annotation"]
-                            and sample["series"] in self.annotations_metadata
-                        )
 
                     dataset.append(sample)
 
@@ -223,26 +221,242 @@ class MGH_Dataset(NLST_Survival_Dataset):
         is_localizer = "LOCALIZER" in literal_eval(series_dict["ImageType"])
         return is_localizer
 
-    @property
-    def METADATA_FILENAME(self):
-        return "MGH_Lung_Fintelmann/mgh_metadata.json"
-
-    @property
-    def task(self):
-        return "CANCER"
-
     @staticmethod
     def set_args(args):
         args.num_classes = args.max_followup
-        args.multi_image = True
 
     def get_summary_statement(self, dataset, split_group):
-        summary = "Constructed MGH CT {} Survival {} dataset with {} records, {} exams, {} patients, and the following class balance \n {}"
+        summary = "Constructed MGH CT Cancer Survival {} dataset with {} records, {} exams, {} patients, and the following class balance \n {}"
         class_balance = Counter([d["y"] for d in dataset])
         exams = set([d["exam"] for d in dataset])
         patients = set([d["pid"] for d in dataset])
         statement = summary.format(
-            self.task,
+            split_group,
+            len(dataset),
+            len(exams),
+            len(patients),
+            class_balance,
+        )
+        statement += "\n" + "Censor Times: {}".format(
+            Counter([d["time_at_event"] for d in dataset])
+        )
+        annotation_msg = (
+            self.annotation_summary_msg(dataset) if self.args.use_annotations else ""
+        )
+        statement += annotation_msg
+        return statement
+
+    def assign_splits(self, meta):
+        for idx in range(len(meta)):
+            meta[idx]["split"] = np.random.choice(
+                ["train", "dev", "test"], p=self.args.split_probs
+            )
+
+
+@register_object("mgh_cohort2", "dataset")
+class MGH_Screening(NLST_Survival_Dataset):
+    """
+    MGH Dataset Cohort 2
+    """
+
+    def create_dataset(self, split_group):
+        """
+        Gets the dataset from the paths and labels in the json.
+        Arguments:
+            split_group(str): One of ['train'|'dev'|'test'].
+        Returns:
+            The dataset as a dictionary with img paths, label,
+            and additional information regarding exam or participant
+        """
+        dataset = []
+
+        # if split probs is set, randomly assign new splits, (otherwise default is 70% train, 15% dev and 15% test)
+        if self.args.assign_splits:
+            np.random.seed(self.args.cross_val_seed)
+            self.assign_splits(self.metadata_json)
+
+        for mrn_row in tqdm(self.metadata_json):
+            if mrn_row["in_cohort1"]:
+                continue
+
+            pid, split, exams = mrn_row["pid"], mrn_row["split"], mrn_row["accessions"]
+
+            if not split == split_group:
+                continue
+
+            for exam_dict in exams:
+
+                for series_id, series_dict in exam_dict["image_series"].items():
+                    if self.skip_sample(series_dict, exam_dict):
+                        continue
+
+                    sample = self.get_volume_dict(
+                        series_id, series_dict, exam_dict, pid
+                    )
+                    if len(sample) == 0:
+                        continue
+
+                    dataset.append(sample)
+
+        return dataset
+
+    def skip_sample(self, series_dict, exam_dict):
+
+        # check if screen is localizer screen or not enough images
+        if self.is_localizer(series_dict["series_data"]):
+            return True
+
+        # check if restricting to specific slice thicknesses
+        slice_thickness = (
+            series_dict["series_data"]["SliceThickness"]
+            if series_dict["series_data"]["SliceThickness"] == ""
+            else float(series_dict["series_data"]["SliceThickness"])
+        )
+
+        if (self.args.slice_thickness_filter is not None) and (
+            (slice_thickness not in self.args.slice_thickness_filter)
+            or (slice_thickness == "")
+        ):
+            return True
+
+        if len(series_dict["series_data"]["paths"]) < self.args.min_num_images:
+            return True
+
+        return False
+
+    def get_volume_dict(self, series_id, series_dict, exam_dict, pid):
+
+        img_paths = series_dict["paths"]
+        img_paths = [
+            p.replace("MIT_Lung_Cancer_Screening", "screening_pngs").replace(
+                ".dcm", ".png"
+            )
+            for p in img_paths
+        ]
+        slice_locations = series_dict["slice_location"]
+        series_data = series_dict["series_data"]
+
+        sorted_img_paths, sorted_slice_locs = self.order_slices(
+            img_paths, slice_locations
+        )
+
+        device = (
+            -1
+            if series_data["Manufacturer"] == -1
+            else DEVICE_ID[series_data["Manufacturer"]]
+        )
+
+        studyuid = exam_dict["StudyInstanceUID"]
+        bridge_uid = exam_dict["bridge_uid"]
+
+        y, y_seq, y_mask, time_at_event = self.get_label(exam_dict)
+
+        sample = {
+            "paths": sorted_img_paths,
+            "slice_locations": sorted_slice_locs,
+            "y": int(y),
+            "time_at_event": time_at_event,
+            "y_seq": y_seq,
+            "y_mask": y_mask,
+            "exam": int(
+                "{}{}".format(
+                    studyuid.replace(".", "")[-5:],
+                    series_id.replace(".", "")[-5:],
+                )
+            ),  # last 5 of study id + last 5 of series id
+            "study": studyuid,
+            "series": series_id,
+            "pid": pid,
+            "bridge_uid": bridge_uid,
+            "device": device,
+            "lung_rads": exam_dict["LR Score"],
+            "cancer_location": np.zeros(14),  # mgh has no annotations
+            "cancer_laterality": np.zeros(
+                3, dtype=np.int
+            ),  # has to be int, while cancer_location has to be float
+            "num_original_slices": len(series_dict["paths"]),
+            "marital_status": exam_dict["marital_status"],
+            "religion": exam_dict["religion"],
+            "primary_site": exam_dict["Primary Site"],
+            "laterality1": exam_dict["Laterality"],
+            "laterality2": exam_dict["Laterality.1"],
+            "icdo3": exam_dict["Histo/Behavior ICD-O-3"],
+        }
+
+        if not self.args.use_all_images:
+            sample["paths"] = fit_to_length(sorted_img_paths, self.args.num_images)
+            sample["slice_locations"] = fit_to_length(
+                sorted_slice_locs, self.args.num_images, "<PAD>"
+            )
+
+        if self.args.use_risk_factors:
+            sample["risk_factors"] = self.get_risk_factors(exam_dict, return_dict=False)
+
+        if self.args.use_annotations:
+            # mgh has no annotations, so set everything to zero / false
+            sample["volume_annotations"] = np.array([0 for _ in sample["paths"]])
+            sample["annotations"] = [
+                {"image_annotations": None} for path in sample["paths"]
+            ]
+        return sample
+
+    def get_label(self, exam_dict):
+
+        is_cancer_cohort = exam_dict["Future_cancer"].lower().strip() == "yes"
+        days_to_cancer = exam_dict["days_before_cancer_dx"]
+        years_to_cancer = int(days_to_cancer // 365)
+
+        y = (is_cancer_cohort) and (years_to_cancer < self.args.max_followup)
+
+        y_seq = np.zeros(self.args.max_followup)
+
+        if y:
+            time_at_event = years_to_cancer
+            y_seq[years_to_cancer:] = 1
+        else:
+            time_at_event = min(
+                years_to_last_followup, self.args.max_followup - 1
+            )  # ! YEARS TO LAST FOLLOWUP
+
+        y_mask = np.array(
+            [1] * (time_at_event + 1)
+            + [0] * (self.args.max_followup - (time_at_event + 1))
+        )
+        y_mask = y_mask[: self.args.max_followup]
+        return y, y_seq.astype("float64"), y_mask.astype("float64"), time_at_event
+
+    def get_risk_factors(self, exam_dict, return_dict=False):
+        risk_factors = {
+            "race": exam_dict["race"],
+            "pack_years": exam_dict["Packs Years"],
+            "age_at_exam": exam_dict["age at the exam"],
+            "gender": exam_dict["gender"],
+            "smoking_status": exam_dict["Smoking Status"],
+            "lung_rads": exam_dict["LR Score"],
+            "years_since_quit_smoking": exam_dict["Year Since Last Smoked"],
+        }
+
+        if return_dict:
+            return risk_factors
+        else:
+            return np.array(
+                [v for v in risk_factors.values() if not isinstance(v, str)]
+            )
+
+    def is_localizer(self, series_dict):
+        is_localizer = "LOCALIZER" in literal_eval(series_dict["ImageType"])
+        return is_localizer
+
+    @staticmethod
+    def set_args(args):
+        args.num_classes = args.max_followup
+
+    def get_summary_statement(self, dataset, split_group):
+        summary = "Constructed MGH CT Cancer Survival {} dataset with {} records, {} exams, {} patients, and the following class balance \n {}"
+        class_balance = Counter([d["y"] for d in dataset])
+        exams = set([d["exam"] for d in dataset])
+        patients = set([d["pid"] for d in dataset])
+        statement = summary.format(
             split_group,
             len(dataset),
             len(exams),
