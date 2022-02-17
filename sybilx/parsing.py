bacbdf95768d7787f7fef76e9988ac0d8f167867
itamarchinn <itamar@chinn.com>
2022-02-17 14:11:46 -0500

    added weighted focal loss args to parsing

--- sybilx/parsing.py
+++ sybilx/parsing.py
@@ -439,16 +439,30 @@ def parse_args(args_strings=None):
     parser.add_argument(
         "--primary_loss_lambda",
         type=float,
-        default=1.0,
+        default=0.9,
         help="Lambda to weigh the primary loss.",
     )
     parser.add_argument(
-        "--adv_loss_lambda",
+        "--wfl_gamma",
         type=float,
-        default=1.0,
+        default=2,
         help="Lambda to weigh the adversary loss.",
     )
 
+    # Weighted Focal Loss
+    parser.add_argument(
+        "--wfl_alpha",
+        type=float,
+        default=1.0,
+        help="Alpha to weigh the WFL loss.",
+    )
+    parser.add_argument(
+        "--wfl_alpha",
+        type=float,
+        default=1.0,
+        help="Alpha to weigh the WFL loss.",
+    )
+    
     # loader
     parser.add_argument(
         "--input_loader_name",
