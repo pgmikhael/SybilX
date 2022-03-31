ff3d31fddc61f79194a0e97029786e370d0569b8
Itamar Chinn <itamarc@mit.edu>
2022-03-31 17:34:10 -0400

    debugging

--- scripts/utils.py
+++ scripts/utils.py
@@ -8,9 +8,11 @@ from datetime import datetime as dt
 
 def get_dataset_stats(args):
     args = copy.deepcopy(args)
+    original_augmentations = get_augmentations( args.train_rawinput_augmentations, args.train_tnsr_augmentations, args )
+    new_augmentations = [a for a in original_augmentations if (a._is_cachable or a.name == 'tensorizer')]
 
     train_data = get_object(args.dataset, 'dataset')(args, 'train')
-    data_loader = get_train_dataset_loader(args, train_data)
+    data_loader = get_train_dataset_loader(args, train_data,  args.batch_size)
 
     means, stds = {i:[] for i in range(args.num_chan)}, {i:[] for i in range(args.num_chan)}
 
@@ -22,7 +24,7 @@ def get_dataset_stats(args):
             means[channel].append(torch.mean(tensor_chan))
             stds[channel].append(torch.std(tensor_chan))
 
-        if indx % 20 == 0 :#(len(data_loader)//20) == 0:
+        if indx % 0 == 0 :#(len(data_loader)//20) == 0:
             _means = [torch.mean(torch.Tensor(means[channel])).item() for channel in range(args.num_chan)]
             _stds = [torch.mean(torch.Tensor(stds[channel])).item() for channel in range(args.num_chan)]
             print('for indx={}\n mean={}\n std={}\n'.format(indx, _means, _stds))
