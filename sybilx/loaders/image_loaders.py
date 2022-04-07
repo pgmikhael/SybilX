f2260ec0c7100db3aeaba3cc03a27c7d5b262110
Ludvig <ludvigkarstens@hotmail.com>
2022-04-06 21:12:37 -0400

    remove windowing from dicom_loader

--- sybilx/loaders/image_loaders.py
+++ sybilx/loaders/image_loaders.py
@@ -337,8 +337,6 @@ class CT16Loader(abstract_loader):
 class DicomLoader(abstract_loader):
     def __init__(self, cache_path, augmentations, args):
         super(DicomLoader, self).__init__(cache_path, augmentations, args)
-        self.window_center = -600
-        self.window_width = 1500
 
     def configure_path(self, path, sample):
         return path
@@ -346,8 +344,7 @@ class DicomLoader(abstract_loader):
     def load_input(self, path, sample):
         try:
             dcm = pydicom.dcmread(path)
-            dcm = apply_modality_lut(dcm.pixel_array, dcm)
-            arr = apply_windowing(dcm, self.window_center, self.window_width)
+            arr = apply_modality_lut(dcm.pixel_array, dcm)
 
             if self.args.use_annotations:
                 mask = get_scaled_annotation_mask(sample["annotations"], self.args)
@@ -361,37 +358,3 @@ class DicomLoader(abstract_loader):
     @property
     def cached_extension(self):
         return ""
-
-
-def apply_windowing(image, center, width, bit_size=16):
-    """Windowing function to transform image pixels for presentation.
-    Must be run after a DICOM modality LUT is applied to the image.
-    Windowing algorithm defined in DICOM standard:
-    http://dicom.nema.org/medical/dicom/2020b/output/chtml/part03/sect_C.11.2.html#sect_C.11.2.1.2
-    Reference implementation:
-    https://github.com/pydicom/pydicom/blob/da556e33b/pydicom/pixel_data_handlers/util.py#L460
-    Args:
-        image (ndarray): Numpy image array
-        center (float): Window center (or level)
-        width (float): Window width
-        bit_size (int): Max bit size of pixel
-    Returns:
-        ndarray: Numpy array of transformed images
-    """
-    y_min = 0
-    y_max = 2**bit_size - 1
-    y_range = y_max - y_min
-
-    c = center - 0.5
-    w = width - 1
-
-    below = image <= (c - w / 2)  # pixels to be set as black
-    above = image > (c + w / 2)  # pixels to be set as white
-    between = np.logical_and(~below, ~above)
-
-    image[below] = y_min
-    image[above] = y_max
-    if between.any():
-        image[between] = ((image[between] - c) / w + 0.5) * y_range + y_min
-
-    return image
