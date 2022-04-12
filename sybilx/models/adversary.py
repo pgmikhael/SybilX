bb3c8b8999d3169f860de13348e1687c1e59bd98
Peter G. Mikhael <pgmikhael@rbgquanta2>
2022-04-12 01:23:44 -0400

    backward compatibility for batchnorm

--- sybilx/models/adversary.py
+++ sybilx/models/adversary.py
@@ -17,10 +17,10 @@ class AlignmentMLP(nn.Module):
         # init discriminator
         self.model = nn.Sequential(
             nn.Linear(discrim_input_size, 512),
-            nn.LayerNorm(512) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(512),
+            nn.LayerNorm(512) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(inplace=True),
             nn.Linear(512, 256),
-            nn.LayerNorm(256) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(256),
+            nn.LayerNorm(256) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(inplace=True),
             nn.Linear(256, args.adv_num_classes),
         )
@@ -50,20 +50,20 @@ class MultiAlignmentMLP(nn.Module):
         # init discriminator
         self.device_model = nn.Sequential(
             nn.Linear(discrim_input_size, 512),
-            nn.LayerNorm(512) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(512),
+            nn.LayerNorm(512) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(inplace=True),
             nn.Linear(512, 256),
-            nn.LayerNorm(256) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(256),
+            nn.LayerNorm(256) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(inplace=True),
             nn.Linear(256, 7),
         )
 
         self.thickness_model = nn.Sequential(
             nn.Linear(discrim_input_size, 512),
-            nn.LayerNorm(512) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(512),
+            nn.LayerNorm(512) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(inplace=True),
             nn.Linear(512, 256),
-            nn.LayerNorm(256) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(256),
+            nn.LayerNorm(256) if args.replace_batchnorm_with_layernorm else nn.BatchNorm1d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(inplace=True),
             nn.Linear(256, 4),
         )
