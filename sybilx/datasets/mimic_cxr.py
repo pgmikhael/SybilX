acc2b60a906f911977a878ab22dec974b142602c
Ludvig <ludvigkarstens@hotmail.com>
2022-04-14 14:37:28 -0400

    implement mimic dataset

--- sybilx/datasets/mimic_cxr.py
+++ sybilx/datasets/mimic_cxr.py
@@ -1,3 +1,4 @@
+import json
 from collections import Counter
 from random import shuffle
 
@@ -6,17 +7,57 @@ from torch.utils import data
 from tqdm import tqdm
 
 from sybilx.utils.registry import register_object
+from sybilx.utils.loading import get_sample_loader
+from sybilx.datasets.nlst_xray import METAFILE_NOTFOUND_ERR
 
-METADATA_PATH = "/Mounts/rbg-storage1/datasets/MIMIC/metadata_jan_2021.json"
+METADATA_PATH = "/Mounts/rbg-storage1/datasets/MIMIC/metadata_feb_2021.json"
 
-SUMMARY_MSG = "Contructed Mimic CXR {} {} dataset with {} records, {} exams, {} patients, and the following class balance \n {}"
+SUMMARY_MSG = "Contructed Mimic CXR {} {} dataset with {} records, {} exams, {} patients"
 CXR_DATASET_NAMES = ["mimic_cxr_opacity", "mimic_cxr_atelectasis", "mimic_cxr_cardiomegaly", "mimic_cxr_consolidation", "mimic_cxr_edema",
                      "mimic_cxr_enlarged_cardiomediastinum", "mimic_cxr_fracture", "mimic_pleural_effusion", "mimic_pleural_other", "mimic_pneumonia", "mimic_pneumothorax"]
-
 
 class Abstract_Mimic_Cxr(data.Dataset):
     '''MIMIC-CXR dataset
     '''
+    def __init__(self, args, split_group):
+        """
+        MIMIC-CXR Dataset
+        params: args - config.
+        params: split_group - ['train'|'dev'|'test'].
+
+        constructs: standard pytorch Dataset obj, which can be fed in a DataLoader for batching
+        """
+        super(Abstract_Mimic_Cxr, self).__init__()
+
+        self.split_group = split_group
+        self.args = args
+
+        # sanity check
+        if args.dataset_file_path != METADATA_PATH:
+            print(f"WARNING! Dataset path set to unexpected path!\nexpected: {METADATA_PATH}\nGot: {args.dataset_file_path}")
+
+        try:
+            self.metadata_json = json.load(open(args.dataset_file_path, "r"))
+        except Exception as e:
+            raise Exception(METAFILE_NOTFOUND_ERR.format(args.dataset_file_path, e))
+
+        self.input_loader = get_sample_loader(split_group, args)
+
+        self.dataset = self.create_dataset(split_group)
+        assert len(self.dataset) != 0
+
+        print(self.get_summary_statement(self.dataset, split_group))
+
+        label_dist = [d[args.class_bal_key] for d in self.dataset]
+        label_counts = Counter(label_dist)
+        weight_per_label = 1.0 / len(label_counts)
+        label_weights = {
+            label: weight_per_label / count for label, count in label_counts.items()
+        }
+
+        print("Class counts are: {}".format(label_counts))
+        print("Label weights are {}".format(label_weights))
+        self.weights = [label_weights[d[args.class_bal_key]] for d in self.dataset]
 
     def create_dataset(self, split_group, img_dir):
         """
@@ -24,14 +65,17 @@ class Abstract_Mimic_Cxr(data.Dataset):
         :split_group: - ['train'|'dev'|'test'].
         :img_dir: - The path to the dir containing the images.
         """
+
+        if self.args.assign_splits:
+            np.random.seed(self.args.cross_val_seed)
+            self.assign_splits(self.metadata_json)
+
         dataset = []
         for row in tqdm(self.metadata_json):
             pid, split, exam = row['pid'], row['split_group'], row['exam']
             split = 'dev' if row['split_group'] == 'validate' else row['split_group']
             if split != split_group:
                 continue
-
-            # TODO
 
             if self.check_label(row):
                 label = self.get_label(row)
@@ -43,16 +87,20 @@ class Abstract_Mimic_Cxr(data.Dataset):
                     'exam': exam,
                 })
 
-        self.input_loader = get_sample_loader(split_group, args) # TODO
-
         return dataset
 
+    def assign_splits(self, meta):
+        assert self.args.split_type == "random"
+        for idx in range(len(meta)):
+            meta[idx]["split"] = np.random.choice(
+                ["train", "dev", "test"], p=self.args.split_probs
+            )
+
     def get_summary_statement(self, dataset, split_group):
-        class_balance = Counter([d['y'] for d in dataset])
         exams = set([d['exam'] for d in dataset])
         patients = set([d['ssn'] for d in dataset])
         statement = SUMMARY_MSG.format(self.task, split_group, len(
-            dataset), len(exams), len(patients), class_balance)
+            dataset), len(exams), len(patients))
         return statement
 
     def get_label(self, row):
@@ -64,8 +112,9 @@ class Abstract_Mimic_Cxr(data.Dataset):
     def __getitem__(self, index):
         sample = self.dataset[index]
 
-        input_dict = self.get_images(sample["paths"], sample)
-        sample.update(input_dict)
+        input_dict = self.input_loader.get_image(sample["path"], sample)
+        sample["x"] = input_dict["input"]
+        #sample["mask"] = input_dict["mask"]
 
         return sample
 
