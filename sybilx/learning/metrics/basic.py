412c1511824b406714d733d9fecf0da3bdf1bca0
Itamar Chinn <itamarc@mit.edu>
2022-06-03 19:34:18 +0100

    impl  multi_task_multi_class_classification metric

--- sybilx/learning/metrics/basic.py
+++ sybilx/learning/metrics/basic.py
@@ -246,3 +246,114 @@ class Survival_Classification(BaseClassification):
                 precision, recall, _ = precision_recall_curve(probs, golds)
                 stats_dict["prauc"] = auc(recall, precision)
         return stats_dict
+
+
+@RegisterMetric("multi_task_multi_class_classification")
+class Multi_Task_Multi_Class_Classification(BaseClassification):
+    def __call__(self, logging_dict, args):
+        stats_dict = OrderedDict()
+
+        prob_keys = [ k[:-5] for k in logging_dict.keys() if k.endswith('probs') ]
+        for key in prob_keys: 
+            golds = logging_dict['{}_golds'.format(key)]
+            probs = logging_dict['{}_probs'.format(key)]
+            preds = preds = torch.argmax(probs, dim=-1).view(-1)
+
+            stats_dict[f"{key}_accuracy"] = accuracy(golds, preds)
+            stats_dict[f"{key}_confusion_matrix"] = confusion_matrix(
+                preds, golds, num_classes
+            )
+            num_classes = probs.shape[1] if len(probs.shape) > 1 else 2
+            if num_classes == 2:
+                if len(probs.shape) == 1:
+                    stats_dict[f"{key}_precision"], stats_dict[f"{key}_recall"] = precision_recall(
+                        probs, golds
+                    )
+                    stats_dict[f"{key}_f1"] = f1(probs, golds)
+                    pr, rc, _ = precision_recall_curve(probs, golds)
+                    stats_dict["pr_auc"] = auc(rc, pr)
+                    try:
+                        stats_dict[f"{key}_roc_auc"] = auroc(probs, golds, pos_label=1)
+                    except:
+                        pass
+                else:
+                    stats_dict[f"{key}_precision"], stats_dict[f"{key}_recall"] = precision_recall(
+                        probs, golds, multiclass=False, num_classes=2
+                    )
+                    stats_dict[f"{key}_f1"] = f1(probs, golds, multiclass=False, num_classes=2)
+                    pr, rc, _ = precision_recall_curve(probs, golds, num_classes=2)
+                    stats_dict[f"{key}_pr_auc"] = auc(rc[-1], pr[-1])
+                    try:
+                        stats_dict[f"{key}_roc_auc"] = auroc(probs, golds, num_classes=2)
+                    except:
+                        pass
+            else:
+                stats_dict[f"{key}_precision"], stats_dict[f"{key}_recall"] = precision_recall(
+                    probs, golds, num_classes=num_classes, average="macro"
+                )
+                stats_dict[f"{key}_f1"] = f1(
+                    probs, golds, num_classes=num_classes, average="macro"
+                )
+                stats_dict[f"{key}_micro_f1"] = f1(
+                    probs, golds, num_classes=num_classes, average="micro"
+                )
+                if len(torch.unique(golds)) == num_classes:
+                    pr, rc, _ = precision_recall_curve(
+                        probs, golds, num_classes=num_classes
+                    )
+                    stats_dict[f"{key}_pr_auc"] = torch.mean(
+                        torch.stack([auc(rc[i], pr[i]) for i in range(num_classes)])
+                    )
+                    stats_dict[f"{key}_roc_auc"] = auroc(
+                        probs, golds, num_classes=num_classes, average="macro"
+                    )
+
+                # store_classwise_metrics not debugged! 
+                if args.store_classwise_metrics:
+                    classwise_metrics = {}
+                    (
+                        classwise_metrics[f"{key}_precisions"],
+                        classwise_metrics[f"{key}_recalls"],
+                    ) = precision_recall(
+                        probs, golds, num_classes=num_classes, average="none"
+                    )
+                    classwise_metrics[f"{key}_f1s"] = f1(
+                        probs, golds, num_classes=num_classes, average="none"
+                    )
+                    pr, rc, _ = precision_recall_curve(
+                        probs, golds, num_classes=num_classes
+                    )
+                    classwise_metrics[f"{key}_pr_aucs"] = [
+                        auc(rc[i], pr[i]) for i in range(num_classes)
+                    ]
+                    classwise_metrics[f"{key}_accs"] = accuracy(
+                        golds, preds, num_classes=num_classes, average="none"
+                    )
+                    try:
+                        classwise_metrics[f"{key}_rocaucs"] = auroc(
+                            probs, golds, num_classes=num_classes, average="none"
+                        )
+                    except:
+                        pass
+
+                    for metricname in [
+                        "precisions",
+                        "recalls",
+                        "f1s",
+                        "rocaucs",
+                        "pr_aucs",
+                        "accs",
+                    ]:
+                        if metricname in classwise_metrics:
+                            stats_dict.update(
+                                {
+                                    "class{}_{}".format(i + 1, metricname): v
+                                    for i, v in enumerate(classwise_metrics[metricname])
+                                }
+                            )
+
+        if len(prob_keys) > 1:
+            stats_dict['mean_accuracy'] = np.mean([stats_dict['{}_accuracy'.format(key)] for key in prob_keys ])
+            stats_dict['mean_roc_auc'] = np.mean([stats_dict['{}_roc_auc'.format(key)] for key in prob_keys ])
+
+        return stats_dict
