7c6cf7036bc09a7ad18915c9712e4d5c405400c4
Itamar Chinn <itamar@chinn.com>
2022-06-06 13:24:37 -0400

    bug fix

--- scripts/data/create_full_plco_xray_metadata_json.py
+++ scripts/data/create_full_plco_xray_metadata_json.py
@@ -23,7 +23,7 @@ parser.add_argument('--split_probs', type = int, nargs = 3, default = DEFAULT_SP
 if __name__ == "__main__":
     args = parser.parse_args()
 
-    tifs = list(path for path in args.tif_dirs.glob('*.tif'))
+    tifs = list(path for path in args.tif_dirs[0].glob('*.tif'))
 
     print("Loading", args.link_csv)
     filename_to_id_map_df = pd.read_csv(args.link_csv)
