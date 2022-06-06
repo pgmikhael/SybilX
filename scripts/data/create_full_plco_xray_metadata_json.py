b5a3192c173546a30fb3f4488e69b75054311555
Itamar Chinn <itamarc@mit.edu>
2022-06-06 13:20:19 -0400

    fixed keys

--- scripts/data/create_full_plco_xray_metadata_json.py
+++ scripts/data/create_full_plco_xray_metadata_json.py
@@ -41,14 +41,9 @@ if __name__ == "__main__":
     for path in loadingbar:
         loadingbar.set_description(f"Processing {path}")
         filename = path.name
-        try:
-            pid = filename_to_id_map[filename]['plco_id']
-            years_from_baseline = filename_to_id_map[filename]['assoc_visit_syr']
-            visit_num = filename_to_id_map[filename]['assoc_visit_visnum']
-        except:
-            pid = filename_to_id_map[filename.replace(".tif", "")]['plco_id']
-            years_from_baseline = filename_to_id_map[filename.replace(".tif", "")]['assoc_visit_syr']
-            visit_num = filename_to_id_map[filename.replace(".tif", "")]['assoc_visit_visnum']
+        pid = filename_to_id_map[filename.replace(".tif", "")]['plco_id']
+        years_from_baseline = filename_to_id_map[filename.replace(".tif", "")]['study_yr']
+        # visit_num = filename_to_id_map[filename.replace(".tif", "")]['assoc_visit_visnum']
         # couldn't find: date, series_id, sop_id
 
         exam = '{}_T{}'.format(visit_num, years_from_baseline)
@@ -56,7 +51,7 @@ if __name__ == "__main__":
 
         exam_dict = {
             "exam": exam,
-            "visit_num": visit_num,
+            # "visit_num": visit_num,
             "study_yr": years_from_baseline,
             #"accession_number": accession_number
             #"date": date
