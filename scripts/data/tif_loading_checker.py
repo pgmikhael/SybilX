7f70ee669b7fb566df55527d9719950afb00535c
Itamar Chinn <itamarc@mit.edu>
2022-06-07 10:31:55 -0400

    checker for tif loading

---
+++ scripts/data/tif_loading_checker.py
@@ -0,0 +1,28 @@
+import json
+import argparse
+from pathlib import Path
+import os
+import cv2
+import skimage.io
+import numpy as np
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--tifs_dirs', type = Path, nargs='*', default="")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    tifs = []
+    for path in args.tifs_dirs:
+        tifs.extend(path.glob('*.tif'))
+
+    loadingbar = tqdm(tifs)
+    for path in loadingbar:
+        loadingbar.set_description(f"Processing {path}")
+        filename = path.name
+        
+        img = skimage.io.imread(str(path), plugin='tifffile')
+        if not img.shape[0] > 0 :
+            print(f"'None' image, path: {path}")
